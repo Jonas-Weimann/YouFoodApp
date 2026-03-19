613ab40b3f517cc2c9aa11f0eb5180e250eae33f
Jonas <jonas.weimann04@gmail.com>
2026-03-19 03:28:44 -0300

    feat: Inicia API con Express

---
+++ backend/src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import { env } from "./config/env.js"
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+
+const PORT = env.PORT || 3000;
+
+// app.use("/api", apiRutas);
+
+app.get("/", (req, res) => {
+  res.status(200).send("API corriendo con exito.");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}.\n`);
+});
