a3ca237f2ad08c02f6e620be91659a4bbddba19b
Jonas <jonas.weimann04@gmail.com>
2026-03-22 22:04:11 -0300

    feat: Implementa inicio de sesión con Google y otros procesos de autenticación

--- frontend/src/components/auth/user-auth-form.jsx
+++ frontend/src/components/auth/user-auth-form.jsx
@@ -1,27 +1,82 @@
 import { Button } from "@/components/ui/button"
-import {
-  Field,
-  FieldGroup,
-  FieldLabel,
-  FieldSeparator,
-} from "@/components/ui/field"
+import { Field, FieldGroup, FieldLabel, FieldSeparator } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
 import { Spinner } from "@/components/ui/spinner"
 import { useState } from "react"
+import { toast } from "sonner"
 
-export function UserAuthForm({...props}) {
+import api from "@/api/api" 
+import { useAuthStore } from "@/hooks/use-auth"
+import { useNavigate } from "react-router-dom"
+import { GoogleLogin } from '@react-oauth/google'
+
+const checkPassword = () => {
+  let password = document.getElementById("password").value;
+  let confirmPassword = document.getElementById("password-confirm").value;
+  let message = document.getElementById("message");
+  if (password.length < 8) {
+  message.innerHTML =
+    "La contraseña debe tener al menos 8 caracteres<br/>";
+  message.style.color = "salmon";
+  return false;
+  }
+  if (password !== confirmPassword) {
+  message.innerHTML = "Las contraseñas no coinciden<br/>";
+  message.style.color = "salmon";
+  return false;
+  }
+  message.innerHTML = "";
+  return true;
+};
+
+export function UserAuthForm({ action, ...props }) {
   const [isLoading, setIsLoading] = useState(false)
+  const setLogin = useAuthStore((state) => state.setLogin)
+  const navigate = useNavigate()
 
   async function onSubmit(event) {
     event.preventDefault()
+    if (!checkPassword()) return;
     setIsLoading(true)
 
-    setTimeout(() => {
+    const formData = new FormData(event.currentTarget)
+    const email = formData.get("email")
+    const password = formData.get("password")
+    const name = formData.get("name")
+
+    try {
+      var data = {}
+      if (action === "Iniciar Sesión"){
+         data = await api.post('/auth/login', { email, password })
+      } else {
+         data = await api.post('/auth/register', { email, name, password })
+      }
+      setLogin(data.token)
+      toast.success(isLogin ? "¡Bienvenido!" : "Cuenta creada con éxito")
+      navigate("/dashboard")
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Error en la conexión")
+      console.error("Error en la petición:", error.response?.data || error.message)
+    } finally {
       setIsLoading(false)
-    }, 3000)
+    }
   }
 
-  if (props.action =="Iniciar Sesión"){
+  const handleGoogleSuccess = async (credentialResponse) => {
+    try {
+      const tokenGoogle = credentialResponse.credential
+      const { data } = await api.post('/auth/google', { token: tokenGoogle })
+      setLogin(data.user, data.token)
+      toast.success("Logueo con Google exitoso")
+      navigate("/dashboard")
+    } catch (error) {
+      console.error("Error en el logueo con Google:", error.response?.data || error.message)
+      toast.error("Error al iniciar con Google")
+    }
+  }
+
+  const isLogin = action === "Iniciar Sesión"
+
     return (
     <div className={("grid gap-6")} {...props}>
       <form onSubmit={onSubmit}>
@@ -32,79 +87,67 @@ export function UserAuthForm({...props}) {
             </FieldLabel>
             <Input
               id="email"
-              placeholder="nombre@dominio.com"
+              placeholder="Correo electrónico"
               type="email"
+              name="email"
               autoCapitalize="none"
               autoComplete="email"
               autoCorrect="off"
+              required
               className={'bg-(--background-light) text-base md:text-base file:text-base text-(--text) placeholder:text-base h-10'}
               disabled={isLoading}
             />
-          </Field>
-          <Field>
-            <Button 
-              disabled={isLoading}
-              className={'bg-(--accent) text-(--background) h-10 text-md hover:bg-(--accent-dimmed) cursor-pointer '}
-            >
-              {isLoading && <Spinner />}
-              Iniciar Sesión
-            </Button>
-          </Field>
-        </FieldGroup>
-      </form>
-      <FieldSeparator className={'text-(--accent-foreground) text-md'}>O continúa con</FieldSeparator>
-      <Button variant="outline" type="button" disabled={isLoading} className={'cursor-pointer border border-(--accent) h-10 text-md text-semibold text-(--accent) hover:bg-(--background-dimmed)'}>
-        {isLoading ? <Spinner /> :  <img src="/src/assets/logo_google.svg" alt="-" className="mr-2 h-4 w-4" />}{" "}
-        Google
-      </Button>
-    </div>
-  )
-  } else {
-    return (
-    <div className={("grid gap-6")} {...props}>
-      <form onSubmit={onSubmit}>
-        <FieldGroup>
-          <Field>
-            <FieldLabel className="sr-only" htmlFor="email">
-              Email
-            </FieldLabel>
-            <Input
-              id="email"
-              placeholder="nombre@dominio.com"
-              type="email"
-              autoCapitalize="none"
-              autoComplete="email"
+              {!isLogin && <Input
+              id="name"
+              placeholder="Nombre"
+              name="name"
+              required
+              autoCapitalize="words"
               autoCorrect="off"
               className={'bg-(--background-light) text-base md:text-base file:text-base text-(--text) placeholder:text-base h-10'}
               disabled={isLoading}
-            />
+            />}
             <Input
               id="password"
               placeholder="Contraseña"
               type="password"
+              name="password"
+              required
               className={'bg-(--background-light) text-base md:text-base file:text-base text-(--text) placeholder:text-base h-10'}
               disabled={isLoading}
             />
+            {!isLogin && <Input
+              id="password-confirm"
+              placeholder="Confirmar contraseña"
+              type="password"
+              required
+              name="password-confirm"
+              className={'bg-(--background-light) text-base md:text-base file:text-base text-(--text) placeholder:text-base h-10'}
+              disabled={isLoading}
+            />}
           </Field>
           <Field>
+            <h2 id="message" className="text-center"></h2>
             <Button 
+              type="submit"
               disabled={isLoading}
-              className={'bg-(--accent) text-(--background) h-10 text-md hover:bg-(--accent-dimmed) cursor-pointer '}
+              className={'bg-(--accent) text-(--background) h-10 text-md hover:bg-(--accent-dimmed) cursor-pointer'}
             >
               {isLoading && <Spinner />}
-              Registrarse
+              {isLogin? 'Iniciar Sesión' : 'Registrarse'}
             </Button>
           </Field>
         </FieldGroup>
       </form>
-      <FieldSeparator className={'text-(--accent-foreground) text-md'}>O continúa con</FieldSeparator>
-      <Button variant="outline" type="button" disabled={isLoading} className={'cursor-pointer border border-(--accent) h-10 text-md text-semibold text-(--accent) hover:bg-(--background-dimmed)'}>
-        {isLoading ? <Spinner /> :  <img src="/src/assets/logo_google.svg" alt="-" className="mr-2 h-4 w-4" />}{" "}
-        Google
-      </Button>
+      <FieldSeparator className={'text-(--accent-foreground) text-md'}>O también puedes</FieldSeparator>
+      <div className="flex justify-center w-full">
+        <GoogleLogin
+          onSuccess={handleGoogleSuccess}
+          onError={() => toast.error("Fallo el login")}
+          theme="filled_black"
+          shape="pill"
+        />
+      </div>
     </div>
   )
-  }
-
-  
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.jsx'
 import './index.css'
 
@@ -13,10 +15,15 @@ const queryClient = new QueryClient({
   },
 })
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_OAUTH_ID_CLIENT}>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </BrowserRouter>  
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
